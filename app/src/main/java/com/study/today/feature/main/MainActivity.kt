ee02f9cd825acf17f1d0dea8f6c697d6e6368a2d
widrift <lolwidriftapp@gmail.com>
2021-05-31 15:42:26 +0900

    버전 1.0.2 제트팩 프레그먼트 추가

--- app/src/main/java/com/study/today/feature/main/MainActivity.kt
+++ app/src/main/java/com/study/today/feature/main/MainActivity.kt
@@ -1,18 +1,72 @@
 package com.study.today.feature.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.study.today.R
-import com.study.today.feature.tour_test.TourFragment
+import com.study.today.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, TourFragment.newInstance())
-                .commitNow()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = findNavController(R.id.main_fragment)
+        setupActionBarWithNavController(navController)
+        setupSmoothBottomMenu()
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_bottom, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.another_item_1 -> {
+                showToast("Another Menu Item 1 Selected")
+            }
+
+            R.id.another_item_2 -> {
+                showToast("Another Menu Item 2 Selected")
+            }
+
+            R.id.another_item_3 -> {
+                showToast("Another Menu Item 3 Selected")
+            }
+
         }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+    private fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setupSmoothBottomMenu() {
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.menu_bottom)
+        val menu = popupMenu.menu
+        binding.bottomBar.setupWithNavController(menu, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
+        /*     if (savedInstanceState == null) {
+                 supportFragmentManager.beginTransaction()
+                     .replace(R.id.container, TourFragment.newInstance())
+                     .commitNow()
+             }*/
